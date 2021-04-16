ca266b41f15e82566d4b932b7485e2b1f387bf6d
Shushanth Madhubalan <mshushanth@gmail.com>
2021-04-16 12:20:52 -0400

    major changes

--- server.py
+++ server.py
@@ -22,4 +22,5 @@ def test():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+    
---
+++ static/js/style.js
@@ -0,0 +1,73 @@
+d3.select("body")
+   .style("background-color"," #d9ccc9")
+
+d3.select("#main")
+	.style("z-index","1")
+	.style("float","left")
+	.style("width","100%")
+	.style("height","100%");
+
+d3.select("#left")
+	.style("width","29%")
+	.style("height","98.5%")
+	.style("margin-top","0.5%")
+	.style("margin-left","0.5%")
+	.style("float","left")
+	.style("background-color","#a89c99");
+
+d3.select("#mid")
+	.style("width","29%")
+	.style("height","98.5%")
+	.style("margin-top","0.5%")
+	.style("margin-left","0.5%")
+	.style("float","left")
+	.style("background-color","#a89c99");
+
+
+d3.select("#right")
+	.style("width","29%")
+	.style("height","98.5%")
+	.style("margin-top","0.5%")
+	.style("margin-left","0.5%")
+	.style("float","left")
+	.style("background-color","#a89c99");
+
+d3.select("#left-top")
+	.style("width","97%")
+	.style("height","58%")
+	.style("margin-bottom","2%")
+
+	
+
+d3.select("#left-bottom")
+	.style("width","97%")
+	.style("height","38%")
+	.style("margin-bottom","2%")
+
+
+    
+d3.select("#mid-top")
+.style("width","97%")
+.style("height","58%")
+.style("margin-bottom","2%")
+
+
+
+d3.select("#mid-bottom")
+.style("width","97%")
+.style("height","38%")
+.style("margin-bottom","2%")
+
+
+d3.select("#right-top")
+.style("width","97%")
+.style("height","58%")
+.style("margin-bottom","2%")
+
+
+
+d3.select("#right-bottom")
+.style("width","97%")
+.style("height","38%")
+.style("margin-bottom","2%")
+
