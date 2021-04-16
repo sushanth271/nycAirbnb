e62248076ef9b674c32e5d23499802dd04a4beb2
Shushanth Madhubalan <mshushanth@gmail.com>
2021-04-16 11:37:31 -0400

    added map.js and few changes in index.html

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+from flask import render_template
+import pandas as pd
+import json
+from sklearn.cluster import KMeans
+import math
+from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
+from sklearn.decomposition import PCA
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route("/")
+def init():
+    return render_template('index.html')
+
+
+@app.route("/test",methods = ['POST', 'GET'])
+def test():
+	return "test message"
+
+
+if __name__ == "__main__":
+    app.run()
